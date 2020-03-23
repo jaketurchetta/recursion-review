620bbe04304560ea1f79934c2f94e6ab457ddf7a
Jake Turchetta <14jturchetta@gmail.com>
2020-03-23 18:59:03 -0400

    min req

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,38 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
+var getElementsByClassName = function(className) {
   // your code here
+  var result = [];
+  var classes = className.split(' ');
+
+
+  var getElementsHelper = function(curr) {
+    var check = true;
+    if (!curr.classList) {
+      check = false;
+    } else {
+      for (var i = 0; i < classes.length; i++) {
+        if (!(curr.classList.contains(classes[i]))) {
+          check = false;
+          break;
+        }
+      }
+    }
+    if (check) {
+      result.push(curr);
+    }
+    if (curr.hasChildNodes()) {
+      let children = curr.childNodes;
+      for (let i = 0; i < children.length; i++) {
+        getElementsHelper(children[i]);
+      }
+    }
+  }
+
+  getElementsHelper(document.body);
+
+  return result;
+
+
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -2,7 +2,52 @@
 // var stringifyJSON = JSON.stringify;
 
 // but you don't so you're going to write it from scratch:
-
+// base cases if in object or array
+var stringifyJSONHelper = function(obj) {
+  var type = typeof(obj);
+  if (type === 'undefined' || type === 'symbol' || type === 'function' || obj === NaN || obj === Infinity || obj === null) {
+    return 'null';
+  } else if (type === 'string') {
+    return '"' + obj + '"';
+  } else if (type === 'number' || type === 'boolean') {
+    return String(obj);
+  } else if (type === 'object') {
+    if (Array.isArray(obj)) {
+      var res = '[';
+      for (var i = 0; i < obj.length; i++) {
+        res += stringifyJSONHelper(obj[i]);
+        res += ',';
+      }
+      if (res.length > 1) {
+        res = res.slice(0,-1);
+      }
+      res += ']';
+      return res;
+    } else {
+      var res = '{';
+      for (var i in obj) {
+        if (stringifyJSON(obj[i]) !== '') {
+          res += stringifyJSONHelper(i);
+          res += ':';
+          res += stringifyJSONHelper(obj[i]);
+          res += ',';
+        }
+      }
+      if (res.length > 1) {
+        res = res.slice(0,-1);
+      }
+      res += '}';
+      return res;
+    }
+  }
+}
+// base cases if not object or array
 var stringifyJSON = function(obj) {
   // your code goes here
+  var type = typeof(obj);
+  if (type === 'undefined' || type === 'symbol' || type === 'function') {
+    return '';
+  } else {
+    return stringifyJSONHelper(obj);
+  }
 };
